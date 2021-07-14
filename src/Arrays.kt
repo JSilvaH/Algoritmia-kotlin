8f2153fc6932f4a1478a6e2eb2353ee4319cf67f
Jesus Silva <jjesussh31@gmail.com>
2021-07-14 16:28:11 -0500

    UniqueCharacters exercise

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -1,3 +1,25 @@
+/*
+    Implement an algorithm to determine if a string has all unique characters. What if
+    you cannot use additional data structures
+*/
+
 fun main(args: Array<String>){
-    print("It's Works")
+    print(isUniqueChars("aa"))
+}
+
+
+
+fun isUniqueChars(str: String): String{
+    if (str.length > 128) return  "Character repeated"
+
+    val map = HashMap<Char, Int>()
+
+    for (i in str.indices){
+        if(map.containsKey(str[i])){
+            return "Character repeated"
+        }else{
+            map[str[i]] = i
+        }
+    }
+    return "No character is  repeated"
 }
