cca008a36a8037fb00a354cfdcdf144d33660033
Jesus Silva <jjesussh31@gmail.com>
2021-07-15 13:05:38 -0500

    BubleSort

---
+++ src/BubbleSort.kt
@@ -0,0 +1,42 @@
+fun main(args: Array<String>){
+    val array = arrayOf(1,3,2,5,8,6,9,4)
+//    printArray(array)
+    myVersionBubbleSort(array)
+    printArray(array)
+}
+
+fun bubbleSort(a: Array<Int>) {
+    val n = a.size
+    (1 until n).map {
+        for (j in 0..n - 1 - it) {
+            if (a[j] > a[j + 1]) {
+                val max = a[j]
+                a[j] = a[j + 1]
+                a[j + 1] = max
+            }
+        }
+
+    }
+
+
+}
+
+fun myVersionBubbleSort(a:Array<Int>){
+    val n = a.size
+    for (i in 1 until n){
+        for (j in 0..n-1-i){
+            if (a[j] > a[j + 1]){
+                val max = a[j]
+                a[j] = a[j + 1]
+                a[j + 1] = max
+            }
+        }
+    }
+}
+
+fun printArray(array: Array<Int>){
+    println("")
+    for(i in array.indices){
+        print("${array[i]}, ")
+    }
+}
